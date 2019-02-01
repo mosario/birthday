b42e73fc7bad784cb5480c462ef8ca4109d1cf7f
pavel.starchikov <pavel.starchikov@simbirsoft.com>
2019-02-01 12:52:45 +0400

    challenge

---
+++ birthday.js
@@ -0,0 +1,11 @@
+;(function () {
+  function HappyBirthDay() {
+    this.congratulations = {
+      health: Number.MAX_VALUE,
+      happiness: Number.MAX_VALUE
+    }
+  }
+
+  return new HappyBirthDay(config);
+
+})(config);
