e90af80cf144ec91117097ebbc9be972a59061b4
muravev <muravevigorek@gmail.com>
2019-02-01 13:52:15 +0400

    add newProduct

--- birthday.js
+++ birthday.js
@@ -1,4 +1,15 @@
 ;(function () {
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  function getNewProduct() {
+    return {
+      name: Math.random().toString(36).substring(3),
+      stack: ['angular', 'vue', 'angular' , 'react', 'angular'][getRandomInt(0,4)],
+    }
+  }
   function HappyBirthDay() {
     this.congratulations = {
       health: Number.MAX_VALUE,
@@ -6,7 +17,8 @@
       teamProductivity: Number.POSITIVE_INFINITY,
       success: true,
       money: Number.MAX_VALUE,
-      message:'we love you!!!'
+      message:'we love you!!!',
+      newProduct: getNewProduct()
     }
   }
 
